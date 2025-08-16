51029ac0c968fdb9672b7c5a6a170ddbd29fc2f7
mary-code217 <xorl0514@naver.com>
2025-08-17 00:26:01 +0900

    security 추가

---
+++ demo/src/main/java/com/example/demo/common/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.demo.common.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+}
